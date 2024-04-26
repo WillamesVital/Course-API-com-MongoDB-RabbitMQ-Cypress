2fc6b510daf84d62ff1769e4d6d39b4c712f843b
WillamesVital <willamescfvital@gmail.com>
2024-04-26 10:56:25 -0300

    Add new file test DELETE

---
+++ mark85/mark85-cypress-api/cypress/e2e/tasks/delete.cy.js
@@ -0,0 +1,51 @@
+const { use } = require("chai")
+
+describe("DELETE /tasks/:id", () => {
+    beforeEach(function () {
+        cy.fixture("tasks/delete").then(function (tasks) {
+            this.tasks = tasks
+        })
+    })
+
+    it("remove a task", function () {
+        const { user, task } = this.tasks.remove
+
+        cy.task("removeTask", task.name, user.email)
+        cy.task("removeUser", user.email)
+        cy.postUser(user)
+
+        cy.postSession(user).then((userResp) => {
+            cy.postTask(task, userResp.body.token).then((taskResp) => {
+                cy.deleteTask(taskResp.body._id, userResp.body.token).then(
+                    (response) => {
+                        expect(response.status).to.eq(204)
+                    }
+                )
+            })
+        })
+    })
+
+    it("tasks not found", function () {
+        const { user, task } = this.tasks.not_found
+
+        cy.task("removeTask", task.name, user.email)
+        cy.task("removeUser", user.email);
+        cy.postUser(user)
+
+        cy.postSession(user).then((userResp) => {
+            cy.postTask(task, userResp.body.token).then((taskResp) => {
+                cy.deleteTask(taskResp.body._id, userResp.body.token).then(
+                    (response) => {
+                        expect(response.status).to.eq(204)
+                    }
+                )
+
+                cy.getUniqueTask(taskResp.body._id, userResp.body.token).then(
+                    (response) => {
+                        expect(response.status).to.eq(404)
+                    }
+                )
+            })
+        })
+    })
+})
